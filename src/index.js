93ccaf0bbbab4012b1daad3b35dfe2d43bc1c0d1
codecaiine <akabrouyannickn@gmail.com>
2022-02-06 10:32:13 +0000

    Add new route

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
