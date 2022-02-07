593efb387a1dce7532422e70e4588097b92a9aaa
codecaiine <akabrouyannickn@gmail.com>
2022-02-07 07:33:56 +0000

    Create course item

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,13 @@
 import { React, useState } from 'react';
-import { Offcanvas } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import RoomItem from '../components/RoomItem';
-import NavPanel from '../components/NavPanel';
 
 function Home() {
-  const rooms = [1, 2, 3, 4, 5];
-  const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
     <>
-      <div className="Container">
-      </div>
+      <div className="Container" />
     </>
   );
 }
