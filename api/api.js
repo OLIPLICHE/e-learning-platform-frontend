913279ad8809782fd8fe69c74920397dd649e0d0
OLIPLICHE <olipliche@gmail.com>
2022-02-08 11:33:58 +0100

    Install Axios for api

---
+++ api/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const END_POINT = 'http://localhost:3000';
+const API_ROUTE = '/api/v1/';
+
+export const api = axios.create({
+    baseURL: `${END_POINT}${API_ROUTE}`,
+});
+
+export const baseApi = axios.create({
+    baseURL: `${END_POINT}`,
+});
