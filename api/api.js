1601f1d8730843459877389b6c772eb66a9b20ff
OLIPLICHE <olipliche@gmail.com>
2022-02-09 07:37:04 +0100

    make get request to the api

--- api/api.js
+++ api/api.js
@@ -5,11 +5,8 @@ const API_ROUTE = '/api/v1/';
 
 export const api = axios.create({
   baseURL: `${END_POINT}${API_ROUTE}`,
-});
-console.log(api.defaults.baseURL);
+}).then((api) => (api.defaults.baseURL));
 
 export const baseApi = axios.create({
   baseURL: `${END_POINT}`,
-});
-
-console.log(baseApi.defaults.baseURL);
+}).then((baseApi) => (baseApi.defaults.baseURL));
