d5d78cbab64ce21022ae8b05fdf6de31d9db54f5
codecaiine <akabrouyannickn@gmail.com>
2022-02-09 21:22:18 +0000

    Add course details

---
+++ src/components/CourseDetails.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Offcanvas } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import NavLeft from './NavLeft';
+
+const CourseDetails = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const coursesDb = {
+    title: 'Course',
+    city: 'test-city',
+    price: '100',
+    description: 'Course description',
+    level: ['level1', 'level2', 'level3', 'level5'],
+  };
+  return (
+    <main className="contain">
+      <div className="vis">
+        <FontAwesomeIcon icon={faBars} onClick={handleShow} />
+      </div>
+      <section className="displaycourse">
+        <div className="nav">
+          <img src={lunar} className="lunar-logo" alt="" />
+          <NavLeft />
+        </div>
+        <div className="details marginFive leftMargin">
+          <div className="displayTwo">
+            <img className="courseImage" src={Image} alt="hotel course" />
+          </div>
+          <div className="displayThree">
+            <h1 className="upperCase">{coursesDb.name}</h1>
+            <p>
+              City:
+              {coursesDb.city}
+            </p>
+            <p>
+              course price: $z
+              {coursesDb.price}
+            </p>
+            <p>
+              description
+              {coursesDb.description}
+            </p>
+            <ul>
+              {coursesDb.levels.map((level) => (
+                <li key={level}>{level}</li>
+              ))}
+            </ul>
+            <button type="button" className="buttonConfig">
+              Add enrolments
+            </button>
+          </div>
+        </div>
+      </section>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Lunar</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </main>
+  );
+};
+
+export default CourseDetails;
