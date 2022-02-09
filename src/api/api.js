fec4d43bd46b72c3a8fff9db14a6b767719530c5
codecaiine <akabrouyannickn@gmail.com>
2022-02-09 21:02:43 +0000

    remove unnecessary function in api

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,9 @@ const API_ROUTE = '/api/v1/';
 
 export const api = axios.create({
   baseURL: `${END_POINT}${API_ROUTE}`,
+  headers: {
+    Authorization: `${localStorage.getItem('token')}`,
+  },
 });
 
 export const baseApi = axios.create({
@@ -15,6 +18,7 @@ export const signup = async (user) => {
   const response = await baseApi.post('/users/signup', { user });
   const authToken = response.headers.authorization;
   const currentUser = response.data;
+  localStorage.setItem('token', authToken);
 
   return { authToken, currentUser };
 };
@@ -24,49 +28,10 @@ export const login = async (user) => {
   const authToken = response.headers.authorization;
   const currentUser = response.data;
 
+  localStorage.setItem('token', authToken);
   return { authToken, currentUser };
 };
 
 export const logout = async () => {
   await api.delete('/users/sign_out');
-};
-
-export const getCourses = async () => {
-  const response = await api.get('/courses');
-  return response.data;
-};
-
-export const getCourse = async (id) => {
-  const response = await api.get(`/courses/${id}`);
-  return response.data;
-};
-
-export const createCourse = async (course) => {
-  const response = await api.post('/courses/', course);
-  return response.data;
-};
-
-export const deleteCourse = async (id) => {
-  const response = await api.delete(`/courses/${id}`);
-  return response.data;
-};
-
-export const createEnrolment = async (enrolment) => {
-  const response = await api.post('/enrolments/', enrolment);
-  return response.data;
-};
-
-export const getEnrolments = async () => {
-  const response = await api.get('/enrolments');
-  return response.data;
-};
-
-export const getEnroloment = async (id) => {
-  const response = await api.get(`/enrolments/${id}`);
-  return response.data;
-};
-
-export const deleteEnrolment = async (id) => {
-  const response = await api.delete(`/enrolments/${id}`);
-  return response.data;
 };
