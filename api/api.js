5113259e80e61028a7145199dcee0f6feb49d8f0
OLIPLICHE <olipliche@gmail.com>
2022-02-09 07:20:16 +0100

    Fix linters

--- api/api.js
+++ api/api.js
@@ -6,10 +6,10 @@ const API_ROUTE = '/api/v1/';
 export const api = axios.create({
   baseURL: `${END_POINT}${API_ROUTE}`,
 });
+console.log(api.defaults.baseURL);
 
 export const baseApi = axios.create({
   baseURL: `${END_POINT}`,
 });
 
-console.log(api.defaults.baseURL);
 console.log(baseApi.defaults.baseURL);
