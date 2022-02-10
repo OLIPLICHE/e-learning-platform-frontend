591148f764f00480d8cbd586785b0c8479abe409
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-02-10 17:07:01 +0000

    Use elector to get enrolments

--- src/components/Enrolment.js
+++ src/components/Enrolment.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Enrolment() {
+function Enrolment(props) {
+    const { enrolment } = props;
     return (
         <div className="enrolment">
           <div className="col-1">
             <div className="check-in">
               <h3>Review</h3>
-              <span>GGG</span>
+              <span>{enrolment.review}</span>
             </div>
             <div className="check-out">
               <h3>Raiting</h3>
-              <span>GGG</span>
+              <span>{enrolment.raiting}</span>
             </div>
           </div>
           <div className="col-2">
             <div className="course-id">
               <h3>course-id:</h3>
-              <span>1258N</span>
+              <span>
+            №
+            {enrolment.id}
+          </span>
             </div>
             <button type="button" className="btn btn-danger">
               Cancel
@@ -26,4 +31,7 @@ function Enrolment() {
       );
 }
 
+Enrolment.propTypes = {
+    enrolment: PropTypes.instanceOf(Object).isRequired,
+  };
 export default Enrolment;
