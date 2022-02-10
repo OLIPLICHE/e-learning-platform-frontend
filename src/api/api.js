b0d44ff889d9f80957acde11883b276cf104898c
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-02-10 20:27:20 +0000

    Finalize api

--- src/api/api.js
+++ src/api/api.js
@@ -5,11 +5,11 @@ const API_ROUTE = '/api/v1/';
 
 export const api = axios.create({
   baseURL: `${END_POINT}${API_ROUTE}`,
-
   headers: {
     Authorization: `${localStorage.getItem('token')}`,
   },
 });
+
 export const baseApi = axios.create({
   baseURL: `${END_POINT}`,
 });
@@ -19,12 +19,15 @@ export const signup = async (user) => {
   const authToken = response.headers.authorization;
   const currentUser = response.data;
   localStorage.setItem('token', authToken);
+
   return { authToken, currentUser };
 };
+
 export const login = async (user) => {
   const response = await baseApi.post('/users/login', { user });
   const authToken = response.headers.authorization;
   const currentUser = response.data;
+
   localStorage.setItem('token', authToken);
   return { authToken, currentUser };
 };
