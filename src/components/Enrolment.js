3b0ba08c5ab75e27b532e8302db0f4cfc33bdb51
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-02-10 16:37:43 +0000

    Add the layout of enrolments

--- src/components/Enrolment.js
+++ src/components/Enrolment.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function Enrolment() {
-  return <div></div>;
+    return (
+        <div className="enrolment">
+          <div className="col-1">
+            <div className="check-in">
+              <h3>Review</h3>
+              <span>GGG</span>
+            </div>
+            <div className="check-out">
+              <h3>Raiting</h3>
+              <span>GGG</span>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="course-id">
+              <h3>course-id:</h3>
+              <span>1258N</span>
+            </div>
+            <button type="button" className="btn btn-danger">
+              Cancel
+            </button>
+          </div>
+        </div>
+      );
 }
 
 export default Enrolment;
