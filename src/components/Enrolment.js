4966127d9df72a0fcdbd0c4b32da3094b36407f8
OLIPLICHE <olipliche@gmail.com>
2022-02-10 19:19:51 +0100

    Update Enrolment file

--- src/components/Enrolment.js
+++ src/components/Enrolment.js
@@ -2,36 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Enrolment(props) {
-    const { enrolment } = props;
-    return (
-        <div className="enrolment">
-          <div className="col-1">
-            <div className="check-in">
-              <h3>Review</h3>
-              <span>{enrolment.review}</span>
-            </div>
-            <div className="check-out">
-              <h3>Raiting</h3>
-              <span>{enrolment.raiting}</span>
-            </div>
-          </div>
-          <div className="col-2">
-            <div className="course-id">
-              <h3>course-id:</h3>
-              <span>
+  const { enrolment } = props;
+  return (
+    <div className="enrolment">
+      <div className="col-1">
+        <div className="check-in">
+          <h3>Review</h3>
+          <span>{enrolment.review}</span>
+        </div>
+        <div className="check-out">
+          <h3>Raiting</h3>
+          <span>{enrolment.raiting}</span>
+        </div>
+      </div>
+      <div className="col-2">
+        <div className="course-id">
+          <h3>course-id:</h3>
+          <span>
             №
             {enrolment.id}
           </span>
-            </div>
-            <button type="button" className="btn btn-danger">
-              Cancel
-            </button>
-          </div>
         </div>
-      );
+        <button type="button" className="btn btn-danger">
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
 
 Enrolment.propTypes = {
-    enrolment: PropTypes.instanceOf(Object).isRequired,
-  };
+  enrolment: PropTypes.instanceOf(Object).isRequired,
+};
 export default Enrolment;
