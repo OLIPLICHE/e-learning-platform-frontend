8b6783d99b68ea3b7546a16a3cf9376f647ff959
codecaiine <akabrouyannickn@gmail.com>
2022-02-10 13:31:04 +0000

    Update courses item

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -3,50 +3,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
-import image from '../images/course.jpg';
+import course from '../images/course.jpg';
 
-const CourseItem = ({ course }) => {
+const CourseItem = ({ course, courses }) => {
   const {
-    title, city, price, description, level, picture,
+    title, city, price, level, description, id,
   } = course;
+
   return (
     <Container className="course-item">
       <Row>
         <Col>
-          <Image className="course-image" src={image} alt="the course" />
+          <Image className="course-image" src={course} alt="course" />
         </Col>
       </Row>
       <Row>
         <Col>
-          {
-            title && <h2>{title}</h2>
-          }
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {
-            city && <h3>{city}</h3>
-          }
-          {
-            price && (
-            <h3>
-              $
+          {title && (
+            <h2>
+              title:
               {' '}
+              {title}
+            </h2>
+          )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {city && (
+            <h3>
+              City Location:
+              {' '}
+              {city}
+            </h3>
+          )}
+          {price && (
+            <h3>
+              price:
+              {' '}
+              $
               {price}
             </h3>
-            )
-          }
-          {
-            description && <h3>{description}</h3>
-          }
-          {
-            level && <h3>{level}</h3>
-          }
-          {
-            picture && <Image className="course-image" src={picture} alt="the course" />
-          }
+          )}
+          {level && (
+            <h3>
+              level:
+              {' '}
+              {level}
+            </h3>
+          )}
+          {description && (
+            <h3>
+              description:
+              {' '}
+              {description}
+            </h3>
+          )}
+          {id && (
+            <Link to={{ pathname: 'course_details/' }}>Click me</Link>
+          )}
         </Col>
       </Row>
     </Container>
@@ -55,6 +72,7 @@ const CourseItem = ({ course }) => {
 
 CourseItem.propTypes = {
   course: PropTypes.instanceOf(Object).isRequired,
+  courses: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default CourseItem;
