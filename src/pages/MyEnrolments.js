572a59bfe9be8976de4975f2a1c4866d0bb248b0
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-02-10 16:22:42 +0000

    Import navLeft to the MyEnrolments

--- src/pages/MyEnrolments.js
+++ src/pages/MyEnrolments.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import course from '../images/course.jpg';
+import NavLeft from '../components/NavLeft';
 
-function MyEnrolment () {
-    return (<h1>My MyEnrolment</h1>  );
-}
+function MyEnrolment() {
+    return (
+      <div className="home">
+        <div className="nav">
+          <img src={course} className="course-logo" alt="" />
+          <NavLeft />
+        </div>
+        <div className="main">
+          <h1>My Enrolments</h1>
+          <h2>
+            You are able to cancel the Enrolment before 24 hours of the
+            Enrolment date
+          </h2>
+        </div>
+      </div>
+    );
+  }
 
-export default MyEnrolment;
+  export default MyEnrolment;
